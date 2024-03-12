1f6dfbd62403f2b38a89a04c7ea63636f9a8eaf3
V-O-L-A-X <110328562+V-O-L-A-X@users.noreply.github.com>
2024-03-12 19:21:38 +0700

    .

--- bao/bao/resources/views/frontend/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<hr>
-    <footer>
-        <div class="row">
-            <div class="col-md-12">
-                <p>Copyright &copy; Your Website 2014</p>
-            </div>
-        </div>
-    </footer>
